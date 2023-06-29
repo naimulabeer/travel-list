8abf8019eb034ec79a6fd224873fbc20ccbadc22
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-29 20:40:16 +0600

    controlled element

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,21 +22,35 @@ function Logo() {
 }
 
 function Form() {
+  const [desc, setDesc] = useState("");
+  const [qty, setQty] = useState(5);
+
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (!desc) return;
+  };
+
+  const handleChange = (event) => {
+    setDesc(event.target.value);
   };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>;
-      <select>
+      <select value={qty} onChange={(e) => setQty(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={desc}
+        onChange={handleChange}
+      />
       <button>Add</button>
     </form>
   );
